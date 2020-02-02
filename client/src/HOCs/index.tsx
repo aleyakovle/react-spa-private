349b66c5603877b1b18d5df65b13c22a42c8f6ea
alexyakovlev <StarScream12w2>
2020-02-02 16:03:56 +0300

    styled-components, react-router installed. hook and hoc for urls.

---
+++ client/src/HOCs/index.tsx
@@ -0,0 +1,10 @@
+import React, {ComponentClass, ComponentType} from 'react';
+import {usePageUrl} from "utils/hooks";
+
+export function withPageNameHOC(RawComponent: ComponentClass | ComponentType) {
+    return (props: any) => {
+        const pageUrl = usePageUrl();
+
+        return <RawComponent pageUrl={pageUrl} {...props} />;
+    };
+}
---
+++ client/src/utils/hooks.ts
@@ -0,0 +1,23 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useLocation} from "react-router";
+import {Dispatch, useMemo} from "react";
+import {IRootState} from "ducks/reducers";
+
+export const useMapState = <T = any>(mapState: (state: IRootState) => T, deps: any[] = []) => {
+    const state: any = useSelector((state) => state);
+
+    return useMemo(() => mapState(state), [state, ...deps]);
+};
+
+export const useMapDispatch = <T = any>(mapDispatch: (dispatch: Dispatch<any>) => T, deps: any[] = []) => {
+    const dispatch = useDispatch();
+    return useMemo(() => mapDispatch(dispatch), [dispatch, ...deps]);
+};
+
+export function usePageUrl() {
+    let location = useLocation();
+
+    return {
+        fullPath: location.pathname,
+    };
+}
