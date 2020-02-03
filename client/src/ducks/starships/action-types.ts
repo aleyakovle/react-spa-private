8ba1c4d59df05ef9408806cbbc424d4b9517a17e
alexyakovlev <StarScream12w2>
2020-02-04 00:31:03 +0300

    unused actions removed

--- client/src/ducks/starships/action-types.ts
+++ client/src/ducks/starships/action-types.ts
@@ -1,7 +1,3 @@
-import {ajaxActionNameCreator, prefixDuckActionName} from "utils/action-helpers";
+import {ajaxActionNameCreator} from "utils/action-helpers";
 
 export const GET_STARSHIPS = ajaxActionNameCreator("GET_STARSHIPS");
-export const GOTO_NEXT_PAGE = prefixDuckActionName("GOTO_NEXT_PAGE");
-export const GOTO_PREV_PAGE = prefixDuckActionName("GOTO_PREV_PAGE");
-export const INCREASE_CURRENT_PAGE = prefixDuckActionName("INCREASE_CURRENT_PAGE");
-export const DECREASE_CURRENT_PAGE = prefixDuckActionName("DECREASE_CURRENT_PAGE");
--- client/src/ducks/starships/actions.ts
+++ client/src/ducks/starships/actions.ts
@@ -1,12 +1,6 @@
 import {
-    DECREASE_CURRENT_PAGE,
-    GET_STARSHIPS, GOTO_NEXT_PAGE, GOTO_PREV_PAGE, INCREASE_CURRENT_PAGE,
+    GET_STARSHIPS
 } from "./action-types";
 import {ajaxActionCreator} from "utils/action-helpers";
-import {createAction} from "redux-actions";
 
 export const getStarships = ajaxActionCreator(GET_STARSHIPS);
-export const gotoNextPage = createAction(GOTO_NEXT_PAGE);
-export const gotoPrevPage = createAction(GOTO_PREV_PAGE);
-export const increasecurrentPageNumber = createAction(INCREASE_CURRENT_PAGE);
-export const decreasecurrentPageNumber = createAction(DECREASE_CURRENT_PAGE);
