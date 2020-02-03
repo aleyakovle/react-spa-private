74d9264e7feb72f4bc4ccc2da46dea5b25b35b9b
alexyakovlev <StarScream12w2>
2020-02-04 02:10:37 +0300

    Skeletons implemented while fetching

--- client/src/components/custom-skeleton/index.tsx
+++ client/src/components/custom-skeleton/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { makeStyles } from '@material-ui/core/styles';
-import {Col, Row} from "react-bootstrap";
-import {StarShipInfoWrapper, StarShipMainWrapper} from "components/styled-components-custom";
+import { Col, Row } from 'react-bootstrap';
+import { StarShipInfoWrapper, StarShipMainWrapper } from 'components/styled-components-custom';
 
 const useStyles = makeStyles({
     root: {
@@ -20,9 +20,7 @@ export const CustomSkeletonTextLine: React.FC<any> = () => {
 };
 
 export const CustomSkeletonImage: React.FC<any> = () => {
-    return (
-        <Skeleton variant="rect" width={'100%'} height={100} />
-    );
+    return <Skeleton variant="rect" width={'100%'} height={'100%'} />;
 };
 
 export const CustomSkeletonStarshipInfo: React.FC<any> = () => {
@@ -37,24 +35,36 @@ export const CustomSkeletonStarshipInfo: React.FC<any> = () => {
     );
 };
 
-export const customSkeletonStarship: React.FC<any> = () => {
-    const classes = useStyles();
+export const CustomSkeletonStarship: React.FC<any> = () => {
     return (
         <StarShipMainWrapper>
             <Row>
-                <Col xs={12} sm={4}>{<CustomSkeletonImage/>}</Col>
+                <Col xs={12} sm={4}>
+                    {<CustomSkeletonImage />}
+                </Col>
                 <Col xs={12} sm={8}>
                     <StarShipInfoWrapper>
-                        <div className={classes.root}>
-                            <Skeleton />
-                            <Skeleton animation={false} />
-                            <Skeleton animation="wave" />
-                            <Skeleton animation={false} />
-                        </div>
+                        <CustomSkeletonStarshipInfo />
                     </StarShipInfoWrapper>
                 </Col>
             </Row>
         </StarShipMainWrapper>
+    );
+};
 
+export const TenSkeletons: React.FC<any> = () => {
+    return (
+        <>
+            <CustomSkeletonStarship />
+            <CustomSkeletonStarship />
+            <CustomSkeletonStarship />
+            <CustomSkeletonStarship />
+            <CustomSkeletonStarship />
+            <CustomSkeletonStarship />
+            <CustomSkeletonStarship />
+            <CustomSkeletonStarship />
+            <CustomSkeletonStarship />
+            <CustomSkeletonStarship />
+        </>
     );
 };
--- client/src/components/starships-list/index.tsx
+++ client/src/components/starships-list/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { IStarship } from 'dtos/starships';
 import { StarShipsListItem } from 'components/starship-list-item';
+import { useSelector } from 'react-redux';
+import { makeGetStarshipsRequestState } from 'ducks/fetching/selectors';
+import { TenSkeletons } from 'components/custom-skeleton';
 
 interface IStarShipsList {
     starships: IStarship[];
@@ -8,25 +11,25 @@ interface IStarShipsList {
 
 export const StarShipsListComponent: React.FC<IStarShipsList> = (props) => {
     const { starships } = props;
+    const { isFetching } = useSelector(makeGetStarshipsRequestState());
 
-    const renderStarships = () => (
-        starships.map((item: IStarship) => (
-            <StarShipsListItem
-                key={item.model + item.name}
-                crew={item.crew}
-                model={item.model}
-                passengers={item.passengers}
-                starshipName={item.name}
-                hyperdrive_rating={item.hyperdrive_rating}
-            />
-        ))
-    );
+    const renderSkeletons = useMemo(() => <TenSkeletons />, [isFetching]);
 
-    return (
-        <div>
-            {renderStarships()}
-        </div>
-    );
+    const renderStarships = () =>
+        isFetching
+            ? renderSkeletons
+            : starships.map((item: IStarship) => (
+                  <StarShipsListItem
+                      key={item.model + item.name}
+                      crew={item.crew}
+                      model={item.model}
+                      passengers={item.passengers}
+                      starshipName={item.name}
+                      hyperdrive_rating={item.hyperdrive_rating}
+                  />
+              ));
+
+    return <div>{renderStarships()}</div>;
 };
 
 export const StarShipsList = StarShipsListComponent;
